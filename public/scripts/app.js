c12824a0da33af365a99f8b841269a811ea530a0
halyosyx <aisle.github@gmail.com>
2021-11-30 17:24:11 +0000

    Implemented pop up functionality and addedscripts

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,8 +4,21 @@ $(document).ready(function() {
 
   console.log('READY');
 
+  const renderCard = function(cards){
+    for (const card of cards) {
+      console.log(card);
+    }
+  }
+
   $('#restaurants, #books, #shows, #products').sortable({
     connectWith: '.layout__list'
   }).disableSelection();
+
+  for (const data of dummyData) {
+    console.log(data);
+  }
+
+  renderCard(dummyData);
+
 
 });
